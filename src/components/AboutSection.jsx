b65197f129dcf5fd424bfcd9df0e119fa67d9e53
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-09-08 18:51:26 -0300

    aboutsection component

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+"use client";
+import React,{ useTransition, useState} from 'react';
+import Image from "next/image";
 
 export const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [startTransition, isPending] = useTransition();
+
+  const handleTabChange = (id) =>{
+    startTransition(()=>{
+      setTab(id);
+    })
+  }
+  
   return (
-    <div>AboutSection</div>
-  )
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-4 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/image/about.jpg" width={400} height={400} />
+        <div>
+          <h2 className="text-4xl font-bold text-white mb-4">Sobre mi</h2>
+          <p className="text-base lg:text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dolor
+            reiciendis odio, porro nemo consequuntur, vero vitae necessitatibus
+            ducimus doloribus optio aliquid labore tempora delectus, nihil
+            quibusdam ad. Doloremque, harum.
+          </p>
+          <div className="flex flex-row mt-8">
+            <span className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500">Skills</span>
+            <span>Educación</span>
+            <span>Experiencia</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,7 @@ export const NavBar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-70 backdrop-blur-sm brightness-90 ">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2 border-b-rose-50">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2  border-b border-dotted shadow-md shadow-slate-900 border-b-indigo-900">
         <Link
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
