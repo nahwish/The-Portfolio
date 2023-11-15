dece99e8a5767f5d515259289c4614dc86a7b1d0
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-11-15 10:50:10 -0300

    added: src immage

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -20,21 +20,27 @@ const AboutSection = () => {
     <>
       <section
         id="about"
-        className="text-white md:grid md:grid-cols-2 gap-4 pt-8 pb-0 "
+        className="text-white md:grid md:grid-cols-3 gap-4 pt-8 pb-0 "
       >
         <Image
-          src="/image/about.jpg"
+          src="/image/personal/about.jpg"
           width={400}
           height={400}
           className="py-10"
           alt="about"
         />
-        <p className=" pt-8 pb-0 py-10">
-          Motivado por encontrar soluciones creativas y altamente efectivas, mi
-          enfoque se centra en la creación de experiencias digitales innovadoras
-          que trasciendan los límites convencionales de la web y el
+        <p className="text-[#ADB7BE] self-center text-base pb-8 sm:text-lg lg:text-lg mb-6 p-10 pb-0 py-10 shadow-inner shadow-black rounded-3xl border-2 bg-[#170b3b3c] border-[#151515]">
+          Mi enfoque se centra en la creación de experiencias digitales
+          innovadoras que trasciendan los límites convencionales de la web y el
           entretenimiento interactivo.
         </p>
+        <Image
+          src="/image/personal/about.jpg"
+          width={400}
+          height={400}
+          className="py-10"
+          alt="about"
+        />
       </section>
       <div className="flex flex-row  justify-center ">
         <TabButton
